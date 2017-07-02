d3d79ebe9bc7ce4d4a027c68fb0205a4eddb3cd3
TINGAUD Frederic <ftingaud@hotmail.com>
2017-07-02 12:57:49 +0200

    Split disassembly between tabs.

--- src/AssemblyEditor.js
+++ src/AssemblyEditor.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import MonacoEditor from 'react-monaco-editor';
 import Palette from './Palette.js';
+import { Tab, Tabs } from 'react-bootstrap';
 
 const RE_CODE = /\s*([0-9\\.]+) +:\s+([0-9a-f]+):\s+(.*)/;
-const RE_TITLE = /-{11} (.*)/;
+const RE_TITLE = /-{11} ([^\s]*)\s*/;
+const RATIO_WIDTH = 7;
+const COMMAND_WIDTH = 7;
 
 class AssemblyEditor extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            code: ''
+            index: 0
+            , lines: []
+            , models: []
+            , titles: []
         }
         this.prevDecorations = [];
     }
     lineNumbersFunc(line) {
-            return this.state.lines[line - 1] || '';
+        return this.state.lines[this.state.index][line - 1] || '';
     }
     editorDidMount(editor, monaco) {
         editor.focus();
@@ -41,50 +47,82 @@ class AssemblyEditor extends React.Component {
             let code = '';
             if (ratio > 0) {
                 code = res[1] + '%';
-                decorations.push({
-                    range: new this.monaco.Range(lines.length + 1, 1, lines.length + 1, code.length + 1),
-                    options: {
-                        inlineClassName: 'ratio-line'
-                    }
-                });
             }
-            code = code + ' '.repeat(7 - code.length) + res[3];
+            code = code + ' '.repeat(RATIO_WIDTH - code.length) + res[3];
+            decorations.push({
+                range: new this.monaco.Range(lines.length + 1, 1, lines.length + 1, RATIO_WIDTH),
+                options: {
+                    inlineClassName: 'ratio-line'
+                }
+            });
             lines.push(res[2]);
             codes.push(code);
             decorations.push({
-                range: new this.monaco.Range(lines.length , 8, lines.length , 15),
+                range: new this.monaco.Range(lines.length, RATIO_WIDTH + 1, lines.length, RATIO_WIDTH + COMMAND_WIDTH + 1),
                 options: {
                     inlineClassName: 'asm-keyword'
                 }
             });
-        } else if (res = string.match(RE_TITLE)) {
-            lines.push(null);
-            codes.push(res[1]);
-            decorations.push({
-                range: new this.monaco.Range(lines.length, 1, lines.length, 6),
-                options: {
-                        isWholeLine: true,
-                        inlineClassName: 'asm-title'
-                }
-            });
         }
     }
-    updateDecorations(decorations) {
-        this.prevDecorations = this.editor.deltaDecorations(
-            this.prevDecorations, decorations);
+    updateDecorations(model, decorations) {
+        model.deltaDecorations(
+            [], decorations);
     }
     makeCode(input) {
         let lines = [];
-        let codes = [];
-        let decorations = [];
-        input.split('\n').map((s, i) => this.splitLine(lines, codes, s, decorations));
+        let models = [];
+        let titles = [];
+        let states = [];
+
+        let blocks = input.split(RE_TITLE);
+        if (blocks[0] === '' && blocks.length % 2 === 1) {
+            for (let i = 1; i < blocks.length; i++) {
+                if (i % 2 === 1) {
+                    titles.push(blocks[i]);
+                } else {
+                    lines.push([]);
+                    states.push(null);
+                    let decorations = [];
+                    let index = lines.length - 1;
+                    let rows = [];
+
+                    blocks[i].split('\n').map(s => this.splitLine(lines[index], rows, s, decorations));
+                    let e = this.monaco.editor;
+                    let model = e.createModel(rows.join('\n'), 'asm');
+                    models.push(model);
+                    this.updateDecorations(model, decorations);
+                }
+            }
+        }
 
         this.setState({
-            lines: lines
-            , code: codes.join('\n')
+            index: 0
+            , lines: lines
+            , models: models
+            , titles: titles
+            , states: states
         });
 
-        this.updateDecorations(decorations);
+        this.editor.setModel(models[0]);
+    }
+    handleSelect(key) {
+            this.state.states[this.state.index] = this.editor.saveViewState();
+        this.setState({
+            index: key
+        });
+
+        this.editor.setModel(this.state.models[key]);
+        this.editor.restoreViewState(this.state.states[key]);
+    }
+    fillTabs() {
+        let tabsList = this.state.titles.map(function (name, i) {
+            return <Tab title={name} eventKey={i} key={name} />
+        });
+
+        return (<Tabs onSelect={(key) => this.handleSelect(key)} defaultActiveKey={0} id="bench-asm-selection">
+            {tabsList}
+        </Tabs>);
     }
     render() {
         const options = {
@@ -94,12 +132,14 @@ class AssemblyEditor extends React.Component {
             , lineNumbersMinChars: 10
         };
         return (
-            <MonacoEditor ref="monaco"
-                language="asm"
-                options={options}
-                editorDidMount={(e, m) => this.editorDidMount(e, m)}
-                value={this.state.code}
-            />
+            <div className="full-size">
+                {this.fillTabs()}
+                <MonacoEditor ref="monaco"
+                    language="asm"
+                    options={options}
+                    editorDidMount={(e, m) => this.editorDidMount(e, m)}
+                />
+            </div>
         );
     }
 }
