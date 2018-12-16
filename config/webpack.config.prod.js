c7e8d833db18628196fa9a6aa19887e55d367da5
Frederic Tingaud <ftingaud@hotmail.com>
2018-12-16 17:54:10 +0100

    Update dependencies

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -240,7 +240,7 @@ module.exports = {
     // <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
     // In production, it will be an empty string unless you specify "homepage"
     // in `package.json`, in which case it will be the pathname of that URL.
-    new InterpolateHtmlPlugin(env.raw),
+    new InterpolateHtmlPlugin(HtmlWebpackPlugin, env.raw),
     // Generates an `index.html` file with the <script> injected.
     new HtmlWebpackPlugin({
       inject: true,
