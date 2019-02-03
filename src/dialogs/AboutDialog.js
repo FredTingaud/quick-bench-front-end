7497a610330c9477330e1b6881df358b2b8127ff
GPSBabel <gpsbabel@users.noreply.github.com>
2019-02-02 21:58:16 -0600

    Update About dialog
    Minor typo and grammar updates to About dialog to make it flow more naturally to a native English speaker.

--- src/dialogs/AboutDialog.js
+++ src/dialogs/AboutDialog.js
@@ -10,14 +10,14 @@ class AboutDialog extends React.Component {
                 </Modal.Header>
                 <Modal.Body>
                     <h4>What is Quick-bench?</h4>
-                    <p>Quick-benchmark is a micro benchmarking tool intended to quickly and simply compare the performances of two or more code snippets.</p>
+                    <p>Quick-benchmark is a micro benchmarking tool intended to quickly and simply compare the performance of two or more code snippets.</p>
 
                     <h4>Why display a ratio of (CPU time / Noop time) instead of actual time?</h4>
-                    <p>The benchmark runs on a pool of AWS machines whose load is unknown, potentialy next to multiple other benchmarks. Any duration it could
+                    <p>The benchmark runs on a pool of AWS machines whose load is unknown and potentialy next to multiple other benchmarks. Any duration it could
                         output would be meaningless. The fact that a snippet takes 100ms to run in quick-bench at a given time gives no information whatsoever
-                        as to what time it will take to run in your application, with your given architecture.</p>
-                    <p>It can however give a reasonnably good comparison between two snippet of code run in the same conditions. It is the purpose this tool
-                        was created for and removing any unit ensures it is what it is used for.</p>
+                        about what time it will take to run in your application, with your given architecture.</p>
+                    <p>Quick-bench can, however, give a reasonably good comparison between two snippets of code run in the same conditions. That is the purpose this tool
+                        was created for; removing any units ensures only meaningful comparison.</p>
                     <p>Using a ratio over an empty function also has another advange: if one of your benchmarks runs as fast as Noop after optimisation, the
                         optimizer probably optimized your code away!</p>
 
