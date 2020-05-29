bc1d0933225a167f9ade319051ca5a0e3a12736a
TINGAUD Frederic <ftingaud@hotmail.com>
2020-05-29 21:12:16 +0200

    Use an outlined button in the wrappable tabs to avoid saturating the space with colors.

--- src/WrappableTabs.js
+++ src/WrappableTabs.js
@@ -52,7 +52,7 @@ class WrappableTabs extends React.Component {
         this.setState({ showTabRename: false });
     }
     renderUnwrapButton() {
-        return <Button onClick={() => this.unwrap()}> <AiOutlineSplitCells /> {this.props.unwrapText}</Button >;
+        return <Button variant="outline-primary" onClick={() => this.unwrap()}> <AiOutlineSplitCells /> {this.props.unwrapText}</Button >;
     }
     rename(t) {
         let titles = this.props.titles;
