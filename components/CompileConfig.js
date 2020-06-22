2cc15475b3b5069c58c55d09289f46d2750eb422
TINGAUD Frederic <ftingaud@hotmail.com>
2020-06-22 15:16:17 +0200

    Use a list of compilers intead of a ton of elseifs

--- components/CompileConfig.js
+++ components/CompileConfig.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { DropdownButton, ButtonToolbar, DropdownItem } from 'react-bootstrap';
+import { Dropdown, ButtonToolbar, DropdownButton } from 'react-bootstrap';
 
 const o0Name = 'None';
 const oGName = 'Og';
@@ -12,27 +12,10 @@ const v11Name = 'c++11';
 const v14Name = 'c++14';
 const v17Name = 'c++17';
 const v20Name = 'c++20';
-const cc38Name = 'clang-3.8';
-const cc39Name = 'clang-3.9';
-const cc40Name = 'clang-4.0';
-const cc50Name = 'clang-5.0';
-const cc60Name = 'clang-6.0';
-const cc70Name = 'clang-7.0';
-const cc71Name = 'clang-7.1';
-const cc80Name = 'clang-8.0';
-const cc90Name = 'clang-9.0';
-const cg55Name = 'gcc-5.5';
-const cg64Name = 'gcc-6.4';
-const cg65Name = 'gcc-6.5';
-const cg72Name = 'gcc-7.2';
-const cg73Name = 'gcc-7.3';
-const cg74Name = 'gcc-7.4';
-const cg75Name = 'gcc-7.5';
-const cg81Name = 'gcc-8.1';
-const cg82Name = 'gcc-8.2';
-const cg83Name = 'gcc-8.3';
-const cg91Name = 'gcc-9.1';
-const cg92Name = 'gcc-9.2';
+const compilers = ['clang-3.8', 'clang-3.9', 'clang-4.0', 'clang-5.0',
+    'clang-6.0', 'clang-7.0', 'clang-7.1', 'clang-8.0', 'clang-9.0',
+    'gcc-5.5', 'gcc-6.4', 'gcc-6.5', 'gcc-7.2', 'gcc-7.3', 'gcc-7.4',
+    'gcc-7.5', 'gcc-8.1', 'gcc-8.2', 'gcc-8.3', 'gcc-9.1', 'gcc-9.2'];
 const lGName = 'libstdc++(GNU)';
 const lCName = 'libc++(LLVM)';
 
@@ -43,53 +26,20 @@ class CompileConfig extends React.Component {
             maxVersion: 20
         };
     }
-    compilerTitle(key) {
-        let compiler = cc38Name;
-        if (key === 'clang-3.9') {
-            compiler = cc39Name;
-        } else if (key === 'clang-4.0') {
-            compiler = cc40Name;
-        } else if (key === 'clang-5.0') {
-            compiler = cc50Name;
-        } else if (key === 'clang-6.0') {
-            compiler = cc60Name;
-        } else if (key === 'clang-7.0') {
-            compiler = cc70Name;
-        } else if (key === 'clang-7.1') {
-            compiler = cc71Name;
-        } else if (key === 'clang-8.0') {
-            compiler = cc80Name;
-        } else if (key === 'clang-9.0') {
-            compiler = cc90Name;
-        } else if (key === 'gcc-5.5') {
-            compiler = cg55Name;
-        } else if (key === 'gcc-6.4') {
-            compiler = cg64Name;
-        } else if (key === 'gcc-6.5') {
-            compiler = cg65Name;
-        } else if (key === 'gcc-7.2') {
-            compiler = cg72Name;
-        } else if (key === 'gcc-7.3') {
-            compiler = cg73Name;
-        } else if (key === 'gcc-7.4') {
-            compiler = cg74Name;
-        } else if (key === 'gcc-7.5') {
-            compiler = cg75Name;
-        } else if (key === 'gcc-8.1') {
-            compiler = cg81Name;
-        } else if (key === 'gcc-8.2') {
-            compiler = cg82Name;
-        } else if (key === 'gcc-8.3') {
-            compiler = cg83Name;
-        } else if (key === 'gcc-9.1') {
-            compiler = cg91Name;
-        } else if (key === 'gcc-9.2') {
-            compiler = cg92Name;
+    compilerName(name) {
+        if (name.startsWith('gcc-')) {
+            return "GCC " + name.substring(4);
+        } else if (name.startsWith('clang-')) {
+            return "Clang " + name.substring(6);
+        } else {
+            return name;
         }
-        if (key.startsWith('gcc') && this.props.value.lib !== 'gnu') {
+    }
+    compilerTitle(key) {
+        if (key.startsWith('gcc-') && this.props.value.lib !== 'gnu') {
             this.changeLib('gnu');
         }
-        return 'compiler = ' + compiler;
+        return 'compiler = ' + this.compilerName(key);
     }
     versionTitle(key) {
         let vName = v11Name;
@@ -169,46 +119,26 @@ class CompileConfig extends React.Component {
         return (
             <ButtonToolbar>
                 <DropdownButton id="compiler" variant="outline-dark" title={this.compilerTitle(compiler)} onSelect={key => this.changeCompiler(key)} className="mr-2">
-                    <DropdownItem eventKey="clang-3.8">{cc38Name}</DropdownItem>
-                    <DropdownItem eventKey="clang-3.9">{cc39Name}</DropdownItem>
-                    <DropdownItem eventKey="clang-4.0">{cc40Name}</DropdownItem>
-                    <DropdownItem eventKey="clang-5.0">{cc50Name}</DropdownItem>
-                    <DropdownItem eventKey="clang-6.0">{cc60Name}</DropdownItem>
-                    <DropdownItem eventKey="clang-7.0">{cc70Name}</DropdownItem>
-                    <DropdownItem eventKey="clang-7.1">{cc71Name}</DropdownItem>
-                    <DropdownItem eventKey="clang-8.0">{cc80Name}</DropdownItem>
-                    <DropdownItem eventKey="clang-9.0">{cc90Name}</DropdownItem>
-                    <DropdownItem eventKey="gcc-5.5" >{cg55Name}</DropdownItem>
-                    <DropdownItem eventKey="gcc-6.4" >{cg64Name}</DropdownItem>
-                    <DropdownItem eventKey="gcc-6.5" >{cg65Name}</DropdownItem>
-                    <DropdownItem eventKey="gcc-7.2" >{cg72Name}</DropdownItem>
-                    <DropdownItem eventKey="gcc-7.3" >{cg73Name}</DropdownItem>
-                    <DropdownItem eventKey="gcc-7.4" >{cg74Name}</DropdownItem>
-                    <DropdownItem eventKey="gcc-7.5" >{cg75Name}</DropdownItem>
-                    <DropdownItem eventKey="gcc-8.1" >{cg81Name}</DropdownItem>
-                    <DropdownItem eventKey="gcc-8.2" >{cg82Name}</DropdownItem>
-                    <DropdownItem eventKey="gcc-8.3" >{cg83Name}</DropdownItem>
-                    <DropdownItem eventKey="gcc-9.1" >{cg91Name}</DropdownItem>
-                    <DropdownItem eventKey="gcc-9.2" >{cg92Name}</DropdownItem>
+                    {compilers.map((name) => <Dropdown.Item eventKey={name}>{this.compilerName(name)}</Dropdown.Item>)}
                 </DropdownButton>
                 <DropdownButton id="language" variant="outline-dark" title={this.versionTitle(cppVersion)} onSelect={key => this.changeVersion(key)} className="mr-2">
-                    <DropdownItem eventKey="11">{v11Name}</DropdownItem>
-                    <DropdownItem eventKey="14">{v14Name}</DropdownItem>
-                    <DropdownItem eventKey="17">{v17Name}</DropdownItem>
-                    <DropdownItem eventKey="20" disabled={maxVersion < 20}>{v20Name}</DropdownItem>
+                    <Dropdown.Item eventKey="11">{v11Name}</Dropdown.Item>
+                    <Dropdown.Item eventKey="14">{v14Name}</Dropdown.Item>
+                    <Dropdown.Item eventKey="17">{v17Name}</Dropdown.Item>
+                    <Dropdown.Item eventKey="20" disabled={maxVersion < 20}>{v20Name}</Dropdown.Item>
                 </DropdownButton>
                 <DropdownButton id="optim" variant="outline-dark" title={this.optimTitle(optim)} onSelect={key => this.changeOptim(key)} className="mr-2">
-                    <DropdownItem eventKey="0">{o0Name}</DropdownItem>
-                    <DropdownItem eventKey="G">{oGName}</DropdownItem>
-                    <DropdownItem eventKey="1">{o1Name}</DropdownItem>
-                    <DropdownItem eventKey="2">{o2Name}</DropdownItem>
-                    <DropdownItem eventKey="S">{oSName}</DropdownItem>
-                    <DropdownItem eventKey="3">{o3Name}</DropdownItem>
-                    <DropdownItem eventKey="F">{oFName}</DropdownItem>
+                    <Dropdown.Item eventKey="0">{o0Name}</Dropdown.Item>
+                    <Dropdown.Item eventKey="G">{oGName}</Dropdown.Item>
+                    <Dropdown.Item eventKey="1">{o1Name}</Dropdown.Item>
+                    <Dropdown.Item eventKey="2">{o2Name}</Dropdown.Item>
+                    <Dropdown.Item eventKey="S">{oSName}</Dropdown.Item>
+                    <Dropdown.Item eventKey="3">{o3Name}</Dropdown.Item>
+                    <Dropdown.Item eventKey="F">{oFName}</Dropdown.Item>
                 </DropdownButton>
                 <DropdownButton id="libc" variant="outline-dark" title={this.libTitle(lib)} onSelect={key => this.changeLib(key)} disabled={compiler.startsWith('gcc')} >
-                    <DropdownItem eventKey="gnu">{lGName}</DropdownItem>
-                    <DropdownItem eventKey="llvm">{lCName}</DropdownItem>
+                    <Dropdown.Item eventKey="gnu">{lGName}</Dropdown.Item>
+                    <Dropdown.Item eventKey="llvm">{lCName}</Dropdown.Item>
                 </DropdownButton>
             </ButtonToolbar>
         );
