62092ad208c1964e42c1e986f3cbbbef99a38e96
TINGAUD Frederic <ftingaud@hotmail.com>
2020-06-27 19:33:38 +0200

    Change favicon author.

--- components/dialogs/ThanksDialog.js
+++ components/dialogs/ThanksDialog.js
@@ -11,10 +11,7 @@ class ThanksDialog extends React.Component {
                 <Modal.Body>
                     The benchmark engine uses <a href='https://github.com/google/benchmark'>Google Benchmark</a>
                     <br />
-                    Favicon <a href='http://www.freepik.com/free-vector/chronometer-timer-collection_717989.htm'>designed by Freepik</a>
-                    <br />
-                    Christmas icon: made by <a href="https://www.flaticon.com/authors/good-ware" title="Good Ware">Good Ware</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">CC 3.0 BY</a>
-
+                    Favicon designed by Lucie Larousse
                 </Modal.Body>
                 <Modal.Footer>
                     <Button onClick={this.props.onHide}>OK</Button>
