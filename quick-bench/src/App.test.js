0fcf0fcff629ff39e2fafcdfcf38c3bf4aad9402
Fred Tingaud <ftingaud@hotmail.com>
2020-07-27 20:20:57 +0200

    Run one empty test for Quick Bench

--- quick-bench/src/App.test.js
+++ quick-bench/src/App.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+jest.mock("react-monaco-editor", () => () => <div></div>);
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
