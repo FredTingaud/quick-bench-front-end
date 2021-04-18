a60a7abb852835ca013ee3560b097ad3191fc7d3
TINGAUD Frederic <ftingaud@hotmail.com>
2021-04-18 20:15:46 +0200

    Add new compilers

--- components/DefaultSettings.js
+++ components/DefaultSettings.js
@@ -1,10 +1,10 @@
 const ALL_COMPILERS = ['clang-3.8', 'clang-3.9', 'clang-4.0', 'clang-5.0',
     'clang-6.0', 'clang-7.0', 'clang-7.1', 'clang-8.0', 'clang-9.0',
-    'clang-10.0', 'gcc-5.5', 'gcc-6.4', 'gcc-6.5', 'gcc-7.2', 'gcc-7.3',
+    'clang-10.0', 'clang-11.0', 'clang-12.0', 'gcc-5.5', 'gcc-6.4', 'gcc-6.5', 'gcc-7.2', 'gcc-7.3',
     'gcc-7.4', 'gcc-7.5', 'gcc-8.1', 'gcc-8.2', 'gcc-8.3', 'gcc-8.4',
-    'gcc-9.1', 'gcc-9.2', 'gcc-9.3', 'gcc-10.1'];
+    'gcc-9.1', 'gcc-9.2', 'gcc-9.3', 'gcc-10.1', 'gcc-10.2', 'gcc-10.3'];
 
 export default {
     allCompilers: ALL_COMPILERS,
-    latestCompiler: 'clang-11.0'
+    latestCompiler: 'clang-12.0'
 };
