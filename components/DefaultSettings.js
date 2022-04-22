af672d291b0af186775028e5f03d8590ae39ec23
Tristan Carel <tristan.carel@gmail.com>
2022-04-22 15:42:26 +0200

    Add gcc 11.3 released on April 21, 2022

--- components/DefaultSettings.js
+++ components/DefaultSettings.js
@@ -3,7 +3,8 @@ const ALL_COMPILERS = ['clang-3.8', 'clang-3.9', 'clang-4.0', 'clang-5.0',
     'clang-10.0', 'clang-11.0', 'clang-11.1', 'clang-12.0', 'clang-13.0',
     'gcc-5.5', 'gcc-6.4', 'gcc-6.5', 'gcc-7.2', 'gcc-7.3',
     'gcc-7.4', 'gcc-7.5', 'gcc-8.1', 'gcc-8.2', 'gcc-8.3', 'gcc-8.4', 'gcc-8.5',
-    'gcc-9.1', 'gcc-9.2', 'gcc-9.3', 'gcc-9.4', 'gcc-10.1', 'gcc-10.2', 'gcc-10.3', 'gcc-11.1', 'gcc-11.2'];
+    'gcc-9.1', 'gcc-9.2', 'gcc-9.3', 'gcc-9.4', 'gcc-10.1', 'gcc-10.2', 'gcc-10.3',
+    'gcc-11.1', 'gcc-11.2', 'gcc-11.3'];
 
 export default {
     allCompilers: ALL_COMPILERS,
